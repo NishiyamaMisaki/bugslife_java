e84bb46c5ecef827cf72739501dbd5a67aac076e
Nishiyama Misaki <misaki.nishiyama@fox-hound.jp>
2023-09-08 18:53:06 +0900

    不要なコードを削除

--- src/main/java/com/example/repository/ShopRepository.java
+++ src/main/java/com/example/repository/ShopRepository.java
@@ -1,15 +1,6 @@
 package com.example.repository;
 
-import java.util.List;
-
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.model.Shop;
 
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-
-	// // nameで曖昧検索ができるよう、クエリメソッドを定義する
-	// public List<Shop> findByNameLike(String name);
-
-	public List<Shop> findAll(Example<Shop> example);
-}
+public interface ShopRepository extends JpaRepository<Shop, Long> {}
